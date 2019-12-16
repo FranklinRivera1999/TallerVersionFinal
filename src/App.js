4cc77e2d8a78b68470aafa3839082d9ff9dc53fb
franklinra1999 <franklinra1999@gmail.com>
2019-12-15 20:14:50 -0500

    v1.8

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-//import { Router, Route, browserHistory } from 'react-router-3';
+//import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Router, Route, browserHistory } from 'react-router-3';
 
 import './App.css';
 
@@ -16,7 +16,7 @@ import TramitesGeneral from './componentes/TramitesGeneral';
 
 function App() {
   return (
-    <Router className="container-fluid">
+    <Router history={browserHistory} className="container-fluid">
       <Route path="/"  exact component={Main}></Route>
       <Route path="/allTramites" className="py-4" exact component={TramitesGeneral}></Route>
     </Router>
